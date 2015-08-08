85cf0a0a26d7a3ed15080d13240af82f12262f62
Atif Hussain <sa31fh@gmail.com>
2015-08-08 18:50:09 -0400

    Add date picker to view entries
    A date picker is now available on both the categories and
    inventory details page.

--- sql_common.php
+++ sql_common.php
@@ -54,7 +54,7 @@ function get_updated_items_count($category_id, $date) {
 }
 
 
-function get_categories() {
+function get_categories($date = null) {
     global $conn;
     connect_to_db();
 
@@ -65,32 +65,52 @@ function get_categories() {
 
     }
 
+    if ($date == null) {
+        $date = date('Y-m-d');
+    }
+
+    echo '<br><form action="sql_common.php" method="post">
+    <input type="hidden" name="func_name" value="get_categories">
+    <input type="date" name="date" value="' .$date. '">
+    <input type="submit"> </form><br>';
+
     echo "<table border=\"1px solid black\">";
     echo "<th>Category</th>";
     echo "<th>Status</th>";
     while ($row = $result->fetch_assoc()) {
         echo '<tr><td><form action="sql_common.php" method="post">
         <input type="hidden" name="func_name" value="get_inventory">
+        <input type="hidden" name="date" value="' .$date. '">
         <input type="hidden" name="category_id" value="' . $row["id"] . '">
         <input type="submit" value="' . $row["name"]. '"></form></td>
-        <td>' .get_updated_items_count($row['id'], date('Y-m-d')). '/' . get_total_items($row['id']) . '</td></tr>';
+        <td>' .get_updated_items_count($row['id'], $date). '/' . get_total_items($row['id']) . '</td></tr>';
     }
     echo "</table>";
 }
 
-function get_inventory($category_id) {
+function get_inventory($category_id, $date) {
     global $conn;
     connect_to_db();
 
-    $sql = 'SELECT Item.name as name, Item.unit as unit, IFNULL(Inventory.amount, "-") as amount
-            from Category INNER JOIN Item ON Category.id = Item.category_id 
-            LEFT JOIN Inventory ON Item.id = Inventory.item_id 
-            WHERE Category.id = ' . $category_id . ' and (Inventory.date = "' . date('Y-m-d') . '" or Inventory.date is null)';
+    $sql = 'SELECT T2.item_name AS name, T2.item_unit AS unit, IFNULL(T1.amount, "-") AS amount FROM
+            (SELECT * from Inventory
+            WHERE Inventory.date = "' .$date. '") AS T1
+            RIGHT JOIN
+            (SELECT Item.id AS item_id, Item.name AS item_name, Item.unit AS item_unit from Item
+            INNER JOIN Category ON Item.category_id = Category.id
+            WHERE Category.id = ' .$category_id. ') AS T2  ON T2.item_id = T1.item_id';
+
     $result = $conn->query($sql); 
     if ($result == False) {
         echo "<br> Query failed <br>";
 
     }
+
+    echo '<br><form action="sql_common.php" method="post">
+    <input type="hidden" name="category_id" value="' .$category_id. '">
+    <input type="hidden" name="func_name" value="get_inventory">
+    <input type="date" name="date" value="' .$date. '">
+    <input type="submit"> </form><br>';
 
     echo "<table border=\"1px solid black\">";
     echo "<th>Item</th>";
@@ -105,7 +125,9 @@ function get_inventory($category_id) {
 }
 
 if (strcmp($_POST['func_name'], 'get_inventory') == 0) {
-    get_inventory($_POST['category_id']);
+    get_inventory($_POST['category_id'], $_POST['date']);
+} else if(strcmp($_POST['func_name'], 'get_categories') == 0) {
+    get_categories($_POST['date']);
 }
 
 ?>
