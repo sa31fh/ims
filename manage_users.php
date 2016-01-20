af14d60352c9ca1aacb3ce9afd5dd57278f8f8f0
Wasif Hussain <swash99@gmail.com>
2016-01-20 16:28:34 +0500

    Replaced headers with captions and other imporvements.
    Signed-off-by: Wasif Hussain <swash99@gmail.com>

--- manage_users.php
+++ manage_users.php
@@ -1,20 +1,19 @@
 <?php
     include "sql_common.php";
-    session_start();
+    session_start(); 
     if (!isset($_SESSION["username"])) {
         header("Location: login.php");
         exit();
     }
     if (isset($_POST["new_username"])) {
         add_new_user($_POST['new_username'], $_POST['new_password'], $_POST['userrole']);
-    }
+    } 
     if(isset($_POST["delete_user"])){
         delete_user($_POST["delete_select"]);
     }
 ?>
 
 <!DOCTYPE html>
-
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -23,55 +22,45 @@
 </head>
 <body>
     <?php include_once "new_nav.php" ?>
-
+    
     <div>
-        <div><h4>Add New User</h4></div>
-        <div class="user_view">
-            <span>UserName</span><br>
-            <span>Password</span><br>
-            <span>UserRole</span>
-        </div>
-        <div class="user_view">
-            <form action="manage_users.php" method="post">
-                <input type="text" name="new_username" required><br/>
-                <input type="password" name="new_password" required><br/>
-                <select name="userrole" class="none">
-                    <?php $result = get_role(); ?>
-                    <?php while ($row = $result->fetch_assoc()): ?>
-                        <option   value="<?php echo $row["role"]?>" > <?php echo $row["role"] ?> </option>
-                    <?php endwhile ?>
-                </select>
-                <input type="submit" value="Add" class="button">
-            </form>
-        </div>
+        <caption>Add New User</caption>
+        <form action="manage_users.php" method="post">
+            <input type="text" name="new_username" placeholder="Username" required><br/>
+            <input type="password" name="new_password" placeholder="Password" required><br/>
+            <label for="userrole">User Role</label><select name="userrole" class="none">
+                <?php $result = get_role(); ?>
+                <?php while ($row = $result->fetch_assoc()): ?>
+                    <option   value="<?php echo $row["role"]?>" > <?php echo $row["role"] ?> </option>
+                <?php endwhile ?>
+            </select>
+            <input type="submit" value="Add" class="button">
+        </form>
     </div>
 
     <div>
-        <div><h4>User List</h4></div>
         <table border="1px">
+            <caption>User List</caption>
             <tr>
                 <th>User</th>
                 <th>Role</th>
             </tr>
-            <?php $result2 = get_users(); ?>
-            <?php while ($userdata = $result2->fetch_assoc()): ?>
+            <?php $result = get_users(); ?>
+            <?php while ($userdata = $result->fetch_assoc()): ?>
                 <tr>
                     <td><?php echo $userdata["username"]; ?> </td>
                     <td>
                         <select name="" id=""class="none">
-                            <?php $result = get_role(); ?>
-                            <?php while ($row = $result->fetch_assoc()): ?>
-                                <option value="<?php $row["role"]?>" > <?php echo $row["role"] ?> </option>
-                            <?php endwhile ?>
+                                <option value="<?php $userdata["role"]?>" > <?php echo $userdata["role"] ?> </option>
                         </select>
                     </td>
                 </tr>
             <?php endwhile ?>
         </table>
     </div>
-
+    
     <div>
-        <div><h4>Delete User</h4></div>
+        <caption>Delete User</caption>
         <form action="manage_users.php" method="post">
             <select name="delete_select" class="none">
                 <?php $result = get_users(); ?>
@@ -82,7 +71,7 @@
             <input type="submit" name="delete_user" value ="Delete" class="button">
         </form>
     </div>
-
+    
 </body>
 </html>
 
