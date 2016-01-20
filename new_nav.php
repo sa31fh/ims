1ba312de40d7bc244d9a6762d314360c3ee82422
Wasif Hussain <swash99@gmail.com>
2016-01-20 16:30:59 +0500

    Added messages tab.
    Signed-off-by: Wasif Hussain <swash99@gmail.com>

--- new_nav.php
+++ new_nav.php
@@ -1,35 +1,29 @@
 <nav>
-    <ul>
-        <li>
-            <a href="category_status.php">Home</a>
+    <ul class="clearfix">
+        <li><a href="category_status.php">Home</a></li>
+        <li><a href="edit_categories.php">Categories</a></li>
+        <li><a href="edit_items.php">Items</a></li>
+        <li><a href="manage_users.php">Users</a></li>
+        <li><a href="messages.php">Messages</a></li>
+    </ul>
+    <ul id="nav_right">
+        <li><a href="print_preview.php">Print Preview</a></li>
+        <li id="userinfo" >
+            <div id="username"><span><?php echo $_SESSION["username"]; ?></span></div>
+            <div id="userrole"><span id="two"><?php echo $_SESSION["userrole"];?></span></div>
+            <ul>
+                <li><a href="change_password.php">Password</a></li>
+            </ul>
         </li>
-        <li>
-            <a href="edit_categories.php">Categories</a>
+        <li id="logout">    
+            <form action="new_nav.php" method="post">
+                <input type="submit" name="logout" value="logout" class ="button">
+            </form>
         </li>
-        <li>
-            <a href="edit_items.php">Items</a>
-        </li>
-        <li>
-            <a href="manage_users.php">Users</a>
-        </li>
-        <ul id="nav_right">
-            <li>
-                <a href="">Print Preview</a>
-            </li>
-            <li id="userinfo" >
-                <div id="username"><span><?php echo $_SESSION["username"]; ?></span></div>
-                <div id="userrole"><span id="two"><?php echo $_SESSION["userrole"];?></span></div>
-            </li>
-            <li id="logout">
-                <form action="new_nav.php" method="post">
-                    <input type="submit" name="logout" value="logout">
-                </form>
-            </li>
-        </ul>
     </ul>
 </nav>
 
-<?php
+<?php 
     if (isset($_POST["logout"])) {
         session_start();
         session_unset();
