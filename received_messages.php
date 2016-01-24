b0e1940e962a59c6000a17e11dbe2eb854894c00
Wasif Hussain <swash99@gmail.com>
2016-01-24 19:36:51 +0500

    This will show all the sent and recieved messages in the "Inbox" tab.
    Each item here is taken as a 'conversation' and each conversation can have many messages within it.
    Clicking on indivigual conversations opens up "message_view.php", showing the messages within that conversation.

---
+++ received_messages.php
@@ -0,0 +1,50 @@
+<?php 
+    include "sql_common.php";
+    session_start();
+    if (!isset($_SESSION["username"])) {
+        header("Location: login.php");
+        exit();
+    } 
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <div>
+        <table class="message_table" id="table">
+            <?php $result = received_conversations($_SESSION["username"]) ?>
+            <?php while ($row = $result->fetch_assoc()): ?>
+                <tr onclick=openMessage(this)>
+                    <td class="name"> <?php if ($row["sender"] == $_SESSION["username"]) {
+                                              echo $row["receiver"];
+                                            }else{ echo $row["sender"]; } ?> </td>
+                    <td class="title"> <?php echo $row["title"] ?></td>
+                    <td class="date"> <?php echo $row["timestamp"] ?></td>
+                    <input type="hidden" name="id" value="<?php echo $row["id"] ?>">
+                </tr>
+            <?php endwhile ?>
+        </table>
+
+        <form action="message_view.php" id="view_message" method="post">
+            <input type="hidden" id="con_id" name="con_id" value="">
+            <input type="hidden" id="receiver_name" name="receiver_name">
+        </form>
+   </div> 
+</body>
+</html>
+
+<script type="text/javascript" src="//code.jquery.com/jquery-1.11.1.js"></script>
+<script>
+    function openMessage(obj){
+        var id = document.getElementById("table").rows[obj.rowIndex].children[3].value;
+        var receiver = document.getElementById("table").rows[obj.rowIndex].children[0].value;
+        document.getElementById("con_id").value = id;
+        document.getElementById("receiver_name").value = receiver;
+        document.getElementById("view_message").submit();
+    }
+</script>
