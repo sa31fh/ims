cae20c3fe3df197d84316fbdebab8afb5b5c5beb
Wasif Hussain <swash99@gmail.com>
2016-01-24 19:29:09 +0500

    Main messages page.
    This file is for the main messages page. The "sidenav" is for accessing different messaging components like the "Inbox" and "Compose" button.
    The files called from the "sideNav" links open in an iframe.
    
    Wasif Hussain <swash99@gmail.com>

--- messages.php
+++ messages.php
@@ -15,16 +15,34 @@
     <link rel="stylesheet" href="styles.css">
 </head>
 <body>
+
+    <ul class="sidenav" id="sideNav">
+        <li><a href="compose_messages.php" target="message_frame" >Compose</a></li>
+        <li><a href="received_messages.php" target="message_frame" >Inbox</a></li>
+        <li><a href="sent_messages.php" target="message_frame">Sent</a></li>
+        <li><a href="">Deleted</a></li>
+    </ul>
     <?php include_once "new_nav.php" ?>
 
-    <div>
-        <form action="" method="post">
-            <input type="text" name="recipient" placeholder="Recipient"><br/>
-            <input type="text" name="title" placeholder="Title"><br/>
-            <textarea name="message" id="" cols="30" rows="10" placeholder="Message"></textarea><br/>
-            <input type="submit" value="Send" class="button">
-        </form>
+    <div class="main_messages">
+        <iframe src="received_messages.php" frameborder="0" name="message_frame" id="message_frame" scrolling="no" onload=adjustHeight(id) ></iframe>
     </div>
-    
+
+    <form action="compose_messages.php" method="post" id="print_form" target="message_frame">
+         <input type="hidden" name="new_print_data" value='<?php if(isset($_POST["print_data"])){echo $_POST["print_data"];}?>'>
+    </form>
 </body>
 </html>
+
+<script type="text/javascript" src="//code.jquery.com/jquery-1.11.1.js"></script>
+<script>
+    function adjustHeight(iframeID){
+        var iframe = document.getElementById(iframeID);
+        var nHeight = iframe.contentWindow.document .body.scrollHeight;
+        iframe.height = (nHeight + 60) + "px";
+    }
+</script>
+
+<?php if (isset($_POST["print_data"])): ?>
+     <script> $(function(){ $("#print_form").submit();  });</script>
+<?php endif ?>
