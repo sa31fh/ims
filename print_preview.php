ec368f57b445e26e5d8333c32a371150bc609b68
Wasif Hussain <swash99@gmail.com>
2016-01-24 19:40:11 +0500

    Added Send Table button.
    The "Send Table" button sends a copy of the table into a new message with can be sent to any user.

--- print_preview.php
+++ print_preview.php
@@ -18,35 +18,50 @@
     <?php $date = date("Y-m-d"); ?>
     <div>
         <a href="category_status.php" class="buttonBack">Back</a>
-    <div>
-    <table>
-        <tr>
-            <th colspan="5"><?php echo date('D, M d Y', strtotime($date)); ?></th>
-        </tr>
-        <?php $current_category = null;
-              $result = print_preview(); ?>
-        <?php while ($row =$result->fetch_assoc()): ?>
-            <?php if ($row["category_name"] != $current_category): ?>
-                <?php $current_category = $row["category_name"] ?>
-                <tr><th colspan="5"><?php echo $current_category ?></th></tr>
-                <tr>
-                    <th>Item</th>
-                    <th>Unit</th>
-                    <th>Quantity Present</th>
-                    <th>Quantity Required</th>
-                    <th>Notes</th>
-                </tr>
-            <?php endif ?>
+        <input type="button" onclick=sendPrint() value="Send Table" class="button">
+    </div>
+
+    <div id="table">
+        <table >
             <tr>
-                <?php $sales_factor = get_expected_sales() / get_base_sales(); ?>
-                <td><?php echo $row["item_name"] ?></td>
-                <td><?php echo $row["unit"] ?></td>
-                <td><?php echo $row["quantity"] ?></td>
-                <td><?php echo (is_numeric($row["quantity"]) ? get_estimated_quantity($sales_factor, $row["item_name"]) - (int)$row["quantity"] : "-") ?></td>
-                <td><?php echo $row["notes"] ?></td>
+                <th colspan="5"><?php echo date('D, M d Y', strtotime($date)); ?></th>
             </tr>
-        <?php endwhile ?>
-    </table>
-    
+            <?php $current_category = null;
+                  $result = print_preview(); ?>
+            <?php while ($row =$result->fetch_assoc()): ?>
+                <?php if ($row["category_name"] != $current_category): ?>
+                    <?php $current_category = $row["category_name"] ?>
+                    <tr><th colspan="5"><?php echo $current_category ?></th></tr>
+                    <tr>
+                        <th>Item</th>
+                        <th>Unit</th>
+                        <th>Quantity Present</th>
+                        <th>Quantity Required</th>
+                        <th>Notes</th>
+                    </tr>
+                <?php endif ?>
+                <tr>
+                    <?php $sales_factor = get_expected_sales() / get_base_sales(); ?>
+                    <td><?php echo $row["item_name"] ?></td>
+                    <td><?php echo $row["unit"] ?></td>
+                    <td><?php echo $row["quantity"] ?></td>
+                    <td><?php echo (is_numeric($row["quantity"]) ? get_estimated_quantity($sales_factor, $row["item_name"]) - (int)$row["quantity"] : "-") ?></td>
+                    <td><?php echo $row["notes"] ?></td>
+                </tr>
+            <?php endwhile ?>
+        </table>
+    </div>
+    <form action="messages.php" id="print_form" method="post">
+        <input type="hidden" id="print_data" name="print_data">
+    </form>
 </body>
 </html>
+
+<script type="text/javascript" src="//code.jquery.com/jquery-1.11.1.js"></script>
+<script>
+    function sendPrint(){
+        var dat = document.getElementById("table").innerHTML;
+        document.getElementById("print_data").value = dat;
+        document.getElementById("print_form").submit();
+    }
+</script>
