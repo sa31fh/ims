465b667933644f1793491bff20d7dd2c7f69cf7f
Wasif Hussain <swash99@gmail.com>
2016-01-29 12:08:48 +0500

    Updated Nav bar.
    Fixed the header bug. Issue #6
    
    Added a date display to show the global date on the nav bar. Added dropdown list in date display for changing the date. Issue #4
    
    Moved the user role within the dropdown list under user. Issue #10
    
    Added Admin Tasks tab and Accounts tab within the user dropdown list.

--- new_nav.php
+++ new_nav.php
@@ -1,18 +1,38 @@
+<?php 
+    if (isset($_POST["logout"])) {
+        session_start();
+        session_unset();
+        session_destroy();
+        header("Location: login.php");
+        exit();
+    }
+    if(isset($_POST["dateview"])) {
+        $_SESSION["date"] = $_POST["dateview"];
+    }
+
+?>
+
 <nav>
     <ul class="clearfix">
-        <li><a href="category_status.php">Home</a></li>
-        <li><a href="edit_categories.php">Categories</a></li>
-        <li><a href="edit_items.php">Items</a></li>
-        <li><a href="manage_users.php">Users</a></li>
-        <li><a href="messages.php">Messages</a></li>
+        <li class=<?php if (isset($page) AND ($page == "home")) {echo "active";} ?>><a href="category_status.php">Home</a></li>
+        <li class=<?php if (isset($page) AND ($page == "messages")) {echo "active";} ?>><a href="messages.php">Messages</a></li>
     </ul>
     <ul id="nav_right">
+        <li><div id="date_display"><?php echo date('D, M d Y', strtotime($_SESSION["date"])); ?></div>
+            <ul id="nav_date">
+                <li><form action="" method="post"><input type="date" name="dateview" onchange="this.form.submit()" value="<?php echo $_SESSION["date"] ?>" ></form></li>
+            </ul>
+        </li>
         <li><a href="print_preview.php">Print Preview</a></li>
         <li id="userinfo" >
             <div id="username"><span><?php echo $_SESSION["username"]; ?></span></div>
-            <div id="userrole"><span id="two"><?php echo $_SESSION["userrole"];?></span></div>
+            <!-- <div id="userrole"><span id="two"><?php echo $_SESSION["userrole"];?></span></div> -->
             <ul>
-                <li><a href="change_password.php">Password</a></li>
+                <li id="userrole_li"><div id="userrole"><?php echo $_SESSION["userrole"] ?></div></li>
+                <li><a href="user_account.php">Account</a></li>
+                <?php if ($_SESSION["userrole"] == "admin"): ?>
+                    <li><a href="admin_tasks.php">Admin Tasks</a></li>
+                <?php endif ?>
             </ul>
         </li>
         <li id="logout">    
@@ -22,13 +42,3 @@
         </li>
     </ul>
 </nav>
-
-<?php 
-    if (isset($_POST["logout"])) {
-        session_start();
-        session_unset();
-        session_destroy();
-        header("Location: login.php");
-        exit();
-    }
-?>
