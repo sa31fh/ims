b6345c280e3f2b8377e5bf267cf9579c15612fcb
Wasif Hussain <swash99@gmail.com>
2016-01-29 12:12:25 +0500

    Change Recipient field to dropdown list.
    Changed the Recipient field from a text field to a dropdown list showing only the avaliable users. Added check to remove session user name from list. Issue #16
    
    Removed some unnecessary code.

--- compose_messages.php
+++ compose_messages.php
@@ -6,7 +6,7 @@
         exit();
     }
     if (isset($_POST["message"])) {
-        new_conversation($_SESSION["username"], $_POST["recipient"], $_POST["title"], $_POST["message"], $_POST["messDate"], $_POST["attached"]);
+        new_conversation($_SESSION["username"], $_POST["recipient"], $_POST["title"], $_POST["message"], gmdate("Y-m-d H:i:s"), $_POST["attached"]);
         header("Location: received_messages.php" );
         exit();
     }
@@ -20,14 +20,18 @@
     <link rel="stylesheet" href="styles.css">
 </head>
 <body>
-    <?php  $date = date("d-m-Y");?>
-
     <div>
         <form action="compose_messages.php" method="post">
-            <input type="text" name="recipient" placeholder="Recipient" required><br/>
+            <select name="recipient" id="recipient">
+                <?php $result = get_users(); ?>
+                <?php while ($row = $result->fetch_assoc()): ?>
+                    <?php if ($row["username"] != $_SESSION["username"]): ?>
+                        <option value="<?php echo $row["username"] ?>"><?php echo $row["username"] ?></option>
+                    <?php endif ?>
+                <?php endwhile ?>
+            </select><br/>
             <input type="text" name="title" placeholder="Title"><br/>
             <textarea name="message" id="" cols="30" rows="10" placeholder="Message" required></textarea><br/>
-            <input type="hidden" name="messDate" value="<?php echo $date ?>"> 
             <input type="submit" value="Send" class="button">
             <?php if (isset($_POST["new_print_data"])): ?>
                 <label for="attached">Attachment</label>
@@ -37,10 +41,3 @@
     </div>
 </body>
 </html>
-
-<script type="text/javascript" src="//code.jquery.com/jquery-1.11.1.js"></script>
-<script>
-    function showTable(){
-        document.getElementById("print_table").innerHTML = document.getElementById("value_holder").value;
-    }
-</script>
