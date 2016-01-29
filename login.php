3d2a7d573adc410999c5ca1a71e90391b063f862
Wasif Hussain <swash99@gmail.com>
2016-01-29 12:04:40 +0500

    Fixed header bug.
    Header bug has been resolved by moving the header function to the top of the file. Issue #6

--- login.php
+++ login.php
@@ -5,6 +5,16 @@
         header("Location: category_status.php");
         exit();
     }
+    if(isset($_POST["username"])){
+        $result = verify_credentials($_POST["username"], $_POST["password"]);
+
+        if ($result == true) {
+            header("Location: category_status.php");
+            exit();
+        } else{
+            echo '<div class="error">Invalid Username or Password</div>';
+        }
+    }
 ?>
 
 <!DOCTYPE html>
@@ -17,23 +27,10 @@
 <body>
     <div>
         <form action="login.php" method="post">
-            <input type="text" name="username" placeholder="Username" required><br/>
-            <input type="password" name="password" placeholder="Password" required><br/>
+            <input class="userinput" type="text" name="username" placeholder="Username" required><br/>
+            <input class="userinput" type="password" name="password" placeholder="Password" required><br/>
             <input type="submit" value="login" class="button">
         </form>
     </div>
 </body>
 </html>
-
-<?php
-    if(isset($_POST["username"])){
-        $result = verify_credentials($_POST["username"], $_POST["password"]);
-
-        if ($result == true) {
-            header("Location: category_status.php");
-            exit();
-        } else{
-            echo '<div class="error">Invalid Username or Password</div>';
-        }
-    }
-?>
