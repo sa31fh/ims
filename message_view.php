5e002e62dec11d9892ba67c94bac140f80d250d4
Wasif Hussain <swash99@gmail.com>
2016-01-31 18:59:41 +0500

    Improved function and variable names.
    Wasif Hussain <swash99@gmail.com>

--- message_view.php
+++ message_view.php
@@ -6,7 +6,7 @@
         exit();
     }
     if (isset($_POST["reply"])) {
-        add_messages($_SESSION["username"], $_POST["receiver_name"], $_POST["message"], $_POST["con_id"], date("d-m-Y"));
+        set_new_message($_SESSION["username"], $_POST["receiver_name"], $_POST["message"], $_POST["conversation_id"], gmdate("Y-m-d H:i:s"));
     }
 ?>
 
@@ -18,20 +18,21 @@
     <link rel="stylesheet" href="styles.css">
 </head>
 <body>
-    <?php $result = get_messages($_POST["con_id"]); ?>
+    <?php $result = get_messages($_POST["conversation_id"]); ?>
     <?php while ($row = $result->fetch_assoc()): ?>
         <div class="message"><?php echo $row["attachment"] ?></div>
         <div class="message">
             <span id="name"><?php echo $row["sender"] ?></span><br/>
             <span id="message"><?php echo $row["message"] ?></span>
-            <span id="time"><?php echo $row["time"] ?></span>
+            <span id="time"><?php echo convert_date_timezone($row["timestamp"]);?></span>
+
         </div>
     <?php endwhile ?>
     <div class="reply_div">
         <form action="message_view.php" method="post">
-            <textarea name="message" id="message_text"></textarea>
-            <input type="submit" name="reply" value="Reply">
-            <input type="hidden" name="con_id" value="<?php echo $_POST["con_id"] ?>">
+            <textarea name="message" id="reply_text"></textarea>
+            <input class="button" type="submit" name="reply" value="Reply">
+            <input type="hidden" name="conversation_id" value="<?php echo $_POST["conversation_id"] ?>">
             <input type="hidden" name="receiver_name" value="<?php echo $_POST["receiver_name"]; ?>">
         </form>
     </div>
