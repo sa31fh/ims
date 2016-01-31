e41275753841d713f61ae81ece2a33aaff7bf1d3
Wasif Hussain <swash99@gmail.com>
2016-01-31 19:05:19 +0500

    Added display for timezone.
    Added a display under the date view in dropdown list to allow a user to see their selected timezone.

--- new_nav.php
+++ new_nav.php
@@ -1,15 +1,14 @@
-<?php 
-    if (isset($_POST["logout"])) {
-        session_start();
-        session_unset();
-        session_destroy();
-        header("Location: login.php");
-        exit();
-    }
-    if(isset($_POST["dateview"])) {
-        $_SESSION["date"] = $_POST["dateview"];
-    }
-
+<?php
+if (isset($_POST["logout"])) {
+    session_start();
+    session_unset();
+    session_destroy();
+    header("Location: login.php");
+    exit();
+}
+if(isset($_POST["dateview"])) {
+    $_SESSION["date"] = $_POST["dateview"];
+}
 ?>
 
 <nav>
@@ -20,13 +19,13 @@
     <ul id="nav_right">
         <li><div id="date_display"><?php echo date('D, M d Y', strtotime($_SESSION["date"])); ?></div>
             <ul id="nav_date">
+                <li id="timezone_li"><span id="timezone"><?php echo $_SESSION["timezone"]; ?></span></li>
                 <li><form action="" method="post"><input type="date" name="dateview" onchange="this.form.submit()" value="<?php echo $_SESSION["date"] ?>" ></form></li>
             </ul>
         </li>
         <li><a href="print_preview.php">Print Preview</a></li>
         <li id="userinfo" >
             <div id="username"><span><?php echo $_SESSION["username"]; ?></span></div>
-            <!-- <div id="userrole"><span id="two"><?php echo $_SESSION["userrole"];?></span></div> -->
             <ul>
                 <li id="userrole_li"><div id="userrole"><?php echo $_SESSION["userrole"] ?></div></li>
                 <li><a href="user_account.php">Account</a></li>
