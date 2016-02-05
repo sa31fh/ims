3c414f10713607abd11cd7299547399ad636b3c0
Wasif Hussain <swash99@gmail.com>
2016-02-05 22:36:46 +0500

    Minor fixes and improvements.
    Improved function update_user_details(). Now uses $current_username and $new_username variables instead of $user. Removed dependency on $_SESSION["username"] variable.
    
    Removed jquery-ajax functions and moved to jq_ajax.php.

--- sql_common.php
+++ sql_common.php
@@ -89,19 +89,18 @@ function get_user_details($user){
         return false;
     }
 }
-function update_user_details($user, $first_name, $last_name, $time_zone){
+function update_user_details($current_username, $new_username, $first_name, $last_name, $time_zone){
     global $conn;
     connect_to_db();
 
     $sql = "UPDATE User 
-            SET username = '$user',
+            SET username = '$new_username',
                 first_name = '$first_name',
                 last_name = '$last_name',
                 time_zone = '$time_zone'
-            WHERE username ='" .$_SESSION['username']. "'";
+            WHERE username ='$current_username'";
 
     if ($result = $conn->query($sql)) {
-        $_SESSION["username"] = $user;
         return true;
     } else {
         echo "<br> update_user Query failed <br>";
@@ -156,25 +155,6 @@ function update_user_role($username, $role) {
         return false;
     }
 }
-/*---------------manage_users.php-------------*/
-if (isset($_POST["newRole"])) {
-     global $conn;
-    connect_to_db();
-
-    $role = $_POST["newRole"];
-    $username = $_POST["UserName"];
-
-    $sql = "UPDATE User 
-            SET userrole_id= (SELECT id FROM UserRole WHERE role='$role') 
-            WHERE username='$username'";
-
-    if ($result = $conn->query($sql)) {
-        return $result; 
-    } else {
-        echo "<br> update_user_role query failed <br>";
-        return false;
-    }
-}
 
 function verify_credentials($username, $password) {
     global $conn;
@@ -219,35 +199,6 @@ function set_session_variables($username){
         }
     } else {
         echo "set_session_variables query failed";
-    }
-}
-
-/*---------user_account.php--------------*/
-if (isset($_POST["userName"])) {
-    global $conn;
-    connect_to_db();
-
-    $username = $_POST["userName"];
-    $password = $_POST["password"];
-
-    $sql = "SELECT * FROM User
-            INNER JOIN UserRole ON User.userrole_id = UserRole.id
-            WHERE username='$username'";
-    
-    if ($result = $conn->query($sql)) {
-        $row = $result->fetch_assoc();
-
-        if ($row == null) {
-            return False;
-        }
-        if (!password_verify($password, $row['password_hash'])) {
-            echo "false";
-        } else {
-            echo "true";
-        }
-    } else {
-        echo "Verify Password Query Failed";
-        return false;
     }
 }
 
@@ -351,7 +302,7 @@ function add_new_item($item_name, $item_unit) {
     $date = date('Y-m-d');
 
     $sql = "SELECT * FROM Item
-            WHERE name = '$item_name' 
+            WHERE name = '{$item_name}' 
             AND deletion_date IS NULL";
 
     if($result = $conn->query($sql)){
@@ -515,28 +466,6 @@ function update_inventory($category_id, $date, $items, $values, $notes) {
     }
 }
 
-if (isset($_POST["itQuan"])) {
-    global $conn;
-    connect_to_db();
-
-    $date = $_POST["itDate"];
-    $item_id = $_POST["itId"];
-    $quantity = $_POST["itQuan"];
-    $item_note = $_POST["itNote"];
-
-    $sql = "INSERT INTO Inventory (`date`, item_id, quantity, notes)
-            VALUES ('$date', '$item_id', '$quantity', '$item_note')
-            ON DUPLICATE KEY UPDATE 
-            `date`= VALUES(`date`), item_id = VALUES(item_id), quantity = VALUES(quantity), notes = VALUES(notes)";
-
-    if ($result = $conn->query($sql)) {
-        return $result; 
-    } else {
-        echo "<br> update_inventory query failed <br>";
-        return false;
-    }
-}
-
 function get_expected_sales() {
     global $conn;
     connect_to_db();
@@ -554,24 +483,6 @@ function get_expected_sales() {
 function update_expected_sales($expected_sales) {
     global $conn;
     connect_to_db();
-    
-    $sql = "INSERT INTO Variables (name, value)  
-            VALUES ('ExpectedSales', '$expected_sales') 
-            ON DUPLICATE KEY UPDATE name = VALUES(name), value = VALUES(value)";
-
-    if ($result = $conn->query($sql)) {
-        return $result; 
-    } else {
-        echo "<br> update_expected_sales query failed <br>";
-        return false;
-    }
-}
-
-if (isset($_POST["sales"])) {
-    global $conn;
-    connect_to_db();
-
-    $expected_sales = $_POST["sales"];
     
     $sql = "INSERT INTO Variables (name, value)  
             VALUES ('ExpectedSales', '$expected_sales') 
@@ -603,25 +514,6 @@ function get_base_quantity($item_name) {
 function update_base_quantity($item_id, $quantity) {
     global $conn;
     connect_to_db();
-
-    $sql = "INSERT INTO BaseQuantity (item_id, quantity)  
-            VALUES ('$item_id' , '$quantity') 
-            ON DUPLICATE KEY UPDATE item_id = VALUES(item_id), quantity = VALUES(quantity)";
-
-    if ($result = $conn->query($sql)) {
-        return $result; 
-    } else {
-        echo "<br> update_base_quantity query failed <br>";
-        return false;
-    }
-}
-
-if (isset($_POST["quantity"])) {
-    global $conn;
-    connect_to_db();
-
-    $quantity = $_POST["quantity"];
-    $item_id = $_POST["item_id"];
 
     $sql = "INSERT INTO BaseQuantity (item_id, quantity)  
             VALUES ('$item_id' , '$quantity') 
@@ -686,27 +578,6 @@ function get_categorized_items($category_name){
     }
 }
 
-if (isset($_POST["categoryName"])) {
-    global $conn;
-    connect_to_db();
-    $category_name = $_POST["categoryName"];
-        
-    $sql = "SELECT Item.name, Item.unit FROM Item 
-            INNER JOIN Category ON Item.category_id = Category.id 
-            WHERE Category.name = '{$category_name}' AND Item.deletion_date IS NULL";
-
-    if ($result = $conn->query($sql)) {
-        echo '<select class="category_select" id="categorized_list" size=8>';
-        while ($row = $result->fetch_assoc()) {
-            echo '<option value="' .$row["name"]. '">' .$row["name"]. ' </option>';
-        }
-        echo '</select>';
-    } else {
-        echo "<br> get_categorized_items query failed <br>";
-        return false;
-    }
-}
-
 function get_uncategorized_items(){
     global $conn;
     connect_to_db();
@@ -741,31 +612,6 @@ function update_items_category($category_name, $items) {
     $result = $conn->query($sql); 
     if ($result == False) {
         echo "<br> Query failed <br>";
-    }
-}
-
-if (isset($_POST["items"])) {
-    global $conn;
-    connect_to_db();
-
-    $category_id = null;
-    $category_name = $_POST["CategoryName"];
-    $items = $_POST["items"];
-
-    if ($category_name != null) {
-        $sql = "SELECT Category.id FROM Category 
-                WHERE Category.name = '$category_name' AND deletion_date IS NULL";
-
-        if ($result = $conn->query($sql)) {
-            $category_id = $result->fetch_assoc()['id'];
-        }
-    }
-    $sql = "UPDATE Item SET category_id =" .($category_id == null ? "null":$category_id). " WHERE name = '$items'";
-    if ($result = $conn->query($sql)) {
-        return $result; 
-    } else {
-        echo "<br> update_items_category query failed <br>";
-        return false;
     }
 }
 
@@ -872,7 +718,7 @@ function get_deleted_conversations($user) {
     }    
 }
 
-function set_new_conversation($sender_name, $receiver_name, $title, $message, $date, $attachment, $sender_status, $receiver_status) {
+function create_new_conversation($sender_name, $receiver_name, $title, $message, $date, $attachment, $sender_status, $receiver_status) {
     global $conn;
     connect_to_db();
 
@@ -947,7 +793,7 @@ function get_messages($conversation_id) {
     }
 }
 
-function set_new_message($sender, $receiver, $message, $conversation_id, $date) {
+function create_new_message($sender, $receiver, $message, $conversation_id, $date) {
     global $conn;
     connect_to_db();
 
@@ -970,16 +816,6 @@ function set_new_message($sender, $receiver, $message, $conversation_id, $date) 
         return false;
     }
 
-}
-
-if (isset($_POST["timeZoneRegion"])) {
-
-    $timezones = array( "Africa"=>"1", "America"=>"2", "Asia"=>"16", "Australia"=>"64", "Europe"=>"128");
-
-    foreach (timezone_identifiers_list($timezones[$_POST["timeZoneRegion"]]) as $tz){
-        $tzs = explode("/", $tz, 2);
-        echo  '<option value="' .$tzs[1]. '">' .$tzs[1]. '</option>' ;
-   }       
 }
 
 function convert_date_timezone($date) {
