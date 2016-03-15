327beed0fd5b48529675499d8978022ba3f115bb
Wasif Hussain <swash99@gmail.com>
2016-03-15 23:35:36 +0500

    Added php documentation. Issue #67
    Php documentation has been added on files base_quantity_table.php and category_table.php

--- database/base_quantity_table.php
+++ database/base_quantity_table.php
@@ -1,10 +1,17 @@
-<?php 
+<?php
 require_once "database_table.php";
 
 class BaseQuantityTable extends DatabaseTable {
 
+    /**
+     * Get the base quantity value for a given item.
+     *
+     * @param  string $item_name name of the item.
+     * @return float quantity    value if query is successful.
+     * @throws Exception         if query fails.
+     */
     public static function get_base_quantity($item_name) {
-        $sql = "SELECT quantity FROM BaseQuantity  
+        $sql = "SELECT quantity FROM BaseQuantity
                 WHERE item_id = (SELECT id FROM Item WHERE name='$item_name')";
 
         if($result = parent::query($sql)) {
@@ -14,6 +21,13 @@ class BaseQuantityTable extends DatabaseTable {
         }
     }
 
+    /**
+     * Set the base quantity for a given item.
+     *
+     * @param string $item_name name of the item.
+     * @param float  $quantity  value of quantity to be set.
+     * @return boolean          returns true on successful query or false if it fails.
+     */
     public static function set_base_quantity($item_name, $quantity) {
         $sql = "INSERT INTO BaseQuantity (item_id, quantity)
                 VALUES( (SELECT id FROM Item WHERE name='$item_name' AND deletion_date is null),
@@ -23,14 +37,28 @@ class BaseQuantityTable extends DatabaseTable {
         return parent::query($sql);
     }
 
+    /**
+     * Update the base quantity value for a give item.
+     *
+     * @param  int  $item_id    id of the item of which the quantity will be updated.
+     * @param  int  $quantity   new quantity value to be updated.
+     * @return boolean          returns true on successful query or false if it fails.
+     */
     public static function update_base_quantity($item_id, $quantity) {
-        $sql = "INSERT INTO BaseQuantity (item_id, quantity)  
-                VALUES ('$item_id', '$quantity') 
+        $sql = "INSERT INTO BaseQuantity (item_id, quantity)
+                VALUES ('$item_id', '$quantity')
                 ON DUPLICATE KEY UPDATE item_id = VALUES(item_id), quantity = VALUES(quantity)";
-                
+
         return parent::query($sql);
     }
-   
+
+    /**
+     * Get estimated quantity for given item.
+     *
+     * @param  float $factor        sales factor needed to calculate the estimate quantity.
+     * @param  string $item_name    item name used to get the base quantity.
+     * @return float                returns the rounded estimated quantity value upto two decimal points.
+     */
     public static function get_estimated_quantity($factor, $item_name) {
         return round(self::get_base_quantity($item_name) * $factor, 2);
     }
--- database/category_table.php
+++ database/category_table.php
@@ -1,23 +1,33 @@
-<?php 
+<?php
 require_once "database_table.php";
 
 class CategoryTable extends DatabaseTable {
 
+    /**
+     * Add a new category or update an existing one.
+     *
+     * Function adds a new category if category name does not exist. If it does exist and was deleted on todays date
+     * the category is updated and its deletion_date set to null.
+     *
+     * @param  string $category_name name of the category.
+     * @return boolean               returns true on query success and false if category already exists.
+     * @throws Exception             if query fails.
+     */
     public static function add_category($category_name) {
         $date = date('Y-m-d');
-        
-        $sql = "SELECT * FROM Category 
+
+        $sql = "SELECT * FROM Category
                 WHERE name = '{$category_name}' AND deletion_date IS NULL";
         $result = parent::query($sql);
         if ($result->num_rows == 0) {
-            $sql = "SELECT * FROM Category 
+            $sql = "SELECT * FROM Category
                     WHERE name = '{$category_name}' AND deletion_date = '{$date}'";
-            $result = parent::query($sql); 
+            $result = parent::query($sql);
             if ($result->num_rows == 0) {
-                $sql = "INSERT INTO Category (name, creation_date) 
+                $sql = "INSERT INTO Category (name, creation_date)
                         VALUES ('{$category_name}', '{$date}')";
             } else {
-                $sql = "UPDATE Category SET deletion_date = NULL 
+                $sql = "UPDATE Category SET deletion_date = NULL
                         WHERE name = '{$category_name}' and deletion_date = '{$date}'";
             }
             if (parent::query($sql)) {
@@ -30,34 +40,56 @@ class CategoryTable extends DatabaseTable {
         }
     }
 
-
+    /**
+     * Delete a category in the database.
+     *
+     * Changes the category "deletion_date" to todays date. If successful, updates all items whos "category_id" matches the id
+     * of the removed category and changes item "category_id"s to NULL.
+     *
+     * @param  string $category_name name of category.
+     * @return boolean               return true if query is succesful and false if it fails.
+     */
     public static function remove_category($category_name) {
-        $sql = "UPDATE Category SET deletion_date = '" .date('Y-m-d'). "' 
+        $sql = "UPDATE Category SET deletion_date = '" .date('Y-m-d'). "'
                 WHERE name = '{$category_name}' and deletion_date IS NULL";
         if ($result = parent::query($sql)) {
-            $sql = "UPDATE Item SET category_id = NULL  
+            $sql = "UPDATE Item SET category_id = NULL
                     WHERE deletion_date IS NULL AND category_id = (SELECT id FROM Category WHERE name='{$category_name}')";
-                    
+
             return parent::query($sql);
         }
     }
 
+    /**
+     * Get categories from the database.
+     *
+     * Returns all the categories that have a "deletion_date" of NULL or greater than todays date.
+     *
+     * @param  string $date       date till which categories will be retrieved.
+     * @return object|false       returns mysqli_result object if data is retrieved or false if query fails.
+     */
     public static function get_categories($date) {
-        $sql = "SELECT * FROM Category 
-                WHERE creation_date <= '{$date}' AND (deletion_date > '{$date}' OR deletion_date IS NULL) 
+        $sql = "SELECT * FROM Category
+                WHERE creation_date <= '{$date}' AND (deletion_date > '{$date}' OR deletion_date IS NULL)
                 ORDER BY name ASC";
 
         return parent::query($sql);
     }
 
+    /**
+     * Get data for print preview table
+     *
+     * @param  string $date     date till which data should be retrieved.
+     * @return object|false     resturns mysqli_result object if data is retrieved or false if query fails.
+     */
     public static function get_print_preview($date) {
-        $sql = "SELECT Category.name as category_name, Item.name as item_name, 
-                    IFNULL(unit, '-') as unit, IFNULL(quantity, '-') as quantity, Inv.notes as notes 
+        $sql = "SELECT Category.name as category_name, Item.name as item_name,
+                    IFNULL(unit, '-') as unit, IFNULL(quantity, '-') as quantity, Inv.notes as notes
                 FROM Category
                 INNER JOIN Item ON Item.category_id = Category.id
-                LEFT OUTER JOIN (SELECT * FROM Inventory WHERE date='{$date}') AS Inv ON Inv.item_id = Item.id 
-                WHERE (Category.creation_date <= '{$date}' AND (Category.deletion_date > '{$date}' OR Category.deletion_date IS NULL)) 
-                AND (Item.creation_date <= '{$date}' AND (Item.deletion_date > '{$date}' OR Item.deletion_date IS NULL)) 
+                LEFT OUTER JOIN (SELECT * FROM Inventory WHERE date='{$date}') AS Inv ON Inv.item_id = Item.id
+                WHERE (Category.creation_date <= '{$date}' AND (Category.deletion_date > '{$date}' OR Category.deletion_date IS NULL))
+                AND (Item.creation_date <= '{$date}' AND (Item.deletion_date > '{$date}' OR Item.deletion_date IS NULL))
                 ORDER BY Category.name, Item.name";
 
         return parent::query($sql);
