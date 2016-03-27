69ce97b113253fb927235ce49ce9f10890a5d2d0
Wasif Hussain <swash99@gmail.com>
2016-03-27 19:49:31 +0500

    Added php documentation. Issue #71

--- utilities.php
+++ utilities.php
@@ -1,4 +1,10 @@
-<?php 
+<?php
+/**
+ * Convert date string to a different timezone.
+ *
+ * @param  string $date     Date value to be converted.
+ * @return string|false     Returns the formated date on success or false if date_format() fails.
+ */
 function convert_date_timezone($date) {
     $newDate = date_create($date, timezone_open('GMT'));
     $tz_date = date_timezone_set($newDate, timezone_open($_SESSION["timezone"]));
