8a64b4a5f4aeca866495847428b2decce466576c
Wasif Hussain <swash99@gmail.com>
2016-05-22 22:33:08 +0500

    Added php documentation.

--- database/timeslot_item_table.php
+++ database/timeslot_item_table.php
@@ -1,8 +1,15 @@
 <?php
 require_once "database_table.php";
 
-Class TimeslotItemTable extends DatabaseTable {
+class TimeslotItemTable extends DatabaseTable {
 
+    /**
+     * Add new item to a timeslot.
+     *
+     * @param string $item_name         Name of new item.
+     * @param string $timeslot_name     Name of timeslot to assign item to.
+     * @return boolean                  Returns true on query success and false if item already exists.
+     */
     public static function add_timeslot_item($item_name, $timeslot_name) {
         $sql = "INSERT INTO TimeSlotItem (item_id, timeslot_id)
                 VALUES ((SELECT id FROM Item WHERE name ='$item_name'),
@@ -11,6 +18,13 @@ Class TimeslotItemTable extends DatabaseTable {
         return parent::query($sql);
     }
 
+    /**
+     * Remove item from timeslot.
+     *
+     * @param  string $item_name     Name of item to remove.
+     * @param  string $timeslot_name Name of timeslot to remove from.
+     * @return boolean               Returns true on query success and false if item already exists.
+     */
     public static function remove_timeslot_item($item_name, $timeslot_name) {
         $sql = "DELETE FROM TimeSlotItem
                 WHERE item_id = (SELECT id FROM Item WHERE name = '$item_name')
@@ -19,10 +33,17 @@ Class TimeslotItemTable extends DatabaseTable {
         return parent::query($sql);
     }
 
-    public static function update_timeslot_factor($tsi_id, $factor) {
+    /**
+     * Update factor for given table id.
+     *
+     * @param  int $timeslot_inventory_id    Id of table row to update.
+     * @param  int $factor                   New factor value.
+     * @return boolean               Returns true on query success and false if item already exists.
+     */
+    public static function update_timeslot_factor($timeslot_inventory_id, $factor) {
         $sql = "UPDATE TimeSlotItem
                 SET factor = '$factor'
-                WHERE id = '$tsi_id'";
+                WHERE id = '$timeslot_inventory_id'";
 
         return parent::query($sql);
     }
--- database/timeslot_table.php
+++ database/timeslot_table.php
@@ -3,6 +3,12 @@ require_once "database_table.php";
 
 class TimeslotTable extends DatabaseTable {
 
+    /**
+     * Add new timeslot
+     *
+     * @param  string $timeslot_name Name of new timeslot
+     * @return boolean               Returns true on query success and false if item already exists.
+     */
     public static function add_timeslot($timeslot_name) {
         $sql = "INSERT INTO TimeSlots (name)
                 VALUES ('$timeslot_name')";
@@ -10,6 +16,11 @@ class TimeslotTable extends DatabaseTable {
         return parent::query($sql);
     }
 
+    /**
+     * Get all timeslots.
+     *
+     * @return object|false     Returns mysqli_result object on query success or false if query fails.
+     */
     public static function get_timeslots() {
         $sql = "SELECT name FROM TimeSlots
                 ORDER BY order_id ASC";
@@ -17,6 +28,12 @@ class TimeslotTable extends DatabaseTable {
         return parent::query($sql);
     }
 
+    /**
+     * Delete a timeslot from the database.
+     *
+     * @param  string $timeslot_name Name of timeslot to delete.
+     * @return boolean               Returns true on query success and false if item already exists.
+     */
     public static function delete_timeslot($timeslot_name) {
         $sql = "DELETE FROM TimeSlots
                 WHERE name = '$timeslot_name'";
@@ -24,6 +41,13 @@ class TimeslotTable extends DatabaseTable {
         return parent::query($sql);
     }
 
+    /**
+     * Update order value for given timeslot.
+     *
+     * @param  string   $timeslot_name  Name of timeslot to update.
+     * @param  int      $order_id       New order value.
+     * @return boolean                  Returns true on query success and false if item already exists.
+     */
     public static function update_timeslot_order($timeslot_name, $order_id) {
         $sql = "UPDATE TimeSlots
                 SET order_id = '$order_id'
