8f08e2bf205c4028b21f7e5a852caa1cba99305f
Wasif Hussain <swash99@gmail.com>
2016-05-22 22:15:22 +0500

    Update compose_messages.php section.
    The compose_messages.php section has been updated to display as a popup window
    instead of a separate page.

--- compose_messages.php
+++ compose_messages.php
@@ -8,9 +8,9 @@ if (!isset($_SESSION["username"])) {
     exit();
 }
 if (isset($_POST["message"])) {
-    ConversationTable::create_conversation($_SESSION["username"], $_POST["recipient"], $_POST["title"], $_POST["message"], gmdate("Y-m-d H:i:s"), isset($_POST["attached"]) ? $_POST["attached"] : null, "read", "unread");
-    header("Location: received_messages.php" );
-    exit();
+    foreach ($_POST["recipient"] as $value) {
+        ConversationTable::create_conversation($_SESSION["username"], $value, $_POST["title"], $_POST["message"], gmdate("Y-m-d H:i:s"), isset($_POST["attached"]) ? $_POST["attached"] : null, "read", "unread");
+    }
 }
 ?>
 
@@ -24,25 +24,78 @@ if (isset($_POST["message"])) {
 </head>
 <body>
     <div class="main_iframe">
-    <div>
-        <form action="compose_messages.php" method="post">
-            <select class="select_user" name="recipient" id="recipient">
-                <?php $result = UserTable::get_users(); ?>
-                <?php while ($row = $result->fetch_assoc()): ?>
-                    <?php if ($row["username"] != $_SESSION["username"]): ?>
-                        <option value="<?php echo $row["username"] ?>"><?php echo $row["first_name"]." ".$row["last_name"]." (".$row["username"].")"; ?></option>
-                    <?php endif ?>
-                <?php endwhile ?>
-            </select><br/>
-            <input type="text" name="title" placeholder="Title"><br/>
-            <textarea name="message" id="" cols="30" rows="10" placeholder="Message" required></textarea><br/>
-            <input type="submit" value="Send" class="button">
+        <form id="compose_form" class="compose_form" onsubmit=submitMessage() action="compose_messages.php" method="post">
+            <div class="compose_recipient">
+                <label>To</label>
+            </div>
+            <div class="div_fade"></div>
+            <div class="compose_title">
+                <div class="name_drawer">
+                    <ul>
+                    <?php $result = UserTable::get_users(); ?>
+                    <?php while ($row = $result->fetch_assoc()): ?>
+                        <?php if ($row["username"] != $_SESSION["username"]): ?>
+                            <li class="contact_li" value="<?php echo $row['username'] ?>"><?php echo $row["first_name"]." ".$row["last_name"]." (".$row["username"].")"; ?></li>
+                        <?php endif ?>
+                    <?php endwhile ?>
+                    </ul>
+                </div>
+                <input type="text" name="title" placeholder="Title">
+            </div>
+            <div class="compose_text">
+                <textarea name="message" placeholder="Message" required></textarea>
+            </div>
+            <div class="compose_attachment">
+                <img src="images/paperclip.png" alt="" width="24px" height="21px">
             <?php if (isset($_POST["new_print_data"])): ?>
-                <label for="attached">Attachment</label>
+                <span id="name"><?php echo '"'.$_POST["print_table_name"].'"'?></span>
+                <span id="date"><?php echo $_POST["print_table_date"] ?></span>
                 <input type="hidden" name="attached" id="attached" value='<?php  echo $_POST["new_print_data"] ?>'>
             <?php endif ?>
+            </div>
+            <div class="compose_toolbar">
+                <input type="submit" class="button"  value="Send">
+            </div>
         </form>
-    </div>
     </div>
 </body>
 </html>
+
+<script type="text/javascript" src="//code.jquery.com/jquery-2.2.0.min.js"></script>
+<script>
+    function submitMessage() {
+        window.parent.location.href = window.parent.location.href;
+    }
+
+    $(document).ready(function() {
+        $(".compose_recipient").click(function() {
+            $(".name_drawer").slideDown(180, "linear");
+            $(".div_fade").css("display", "block");
+        });
+
+        $(".contact_li").click(function() {
+            var contact = $(this).html();
+            $(this).toggleClass(function() {
+              if ($(this).hasClass("selected")) {
+                $(".name_tag").each(function() {
+                    if ($(this).children("span").html() == contact) {
+                        $(this).remove();
+                    }
+                });
+              } else {
+                var span = "<div class='name_tag'>"+
+                               "<span>"+contact+"</span>"+
+                               "<input type='hidden' name='recipient[]' value='"+$(this).attr("value")+"'>"+
+                           "</div>"
+                $(".compose_recipient").append(span);
+              }
+              return "selected";
+            });
+        });
+
+        $(".div_fade").click(function() {
+            $(".div_fade").css("display", "none");
+            $(".name_drawer").slideUp(180, "linear");
+        });
+    });
+</script>
--- messages.php
+++ messages.php
@@ -16,7 +16,7 @@ if (!isset($_SESSION["username"])) {
 </head>
 <body>
     <ul class="sidenav" id="sideNav">
-        <li><a id="compose" href="compose_messages.php" target="message_frame" >Compose</a></li>
+        <li id="compose_li"><button class="button" onclick=composeButton()>Compose</button></li>
         <li><a id="inbox" class="active" href="received_messages.php" target="message_frame" >Inbox <span id="status_view"></span></a></li>
         <li><a id="deleted" href="deleted_messages.php" target="message_frame">Deleted</a></li>
     </ul>
@@ -24,7 +24,16 @@ if (!isset($_SESSION["username"])) {
           include_once "new_nav.php" ?>
 
     <div class="main_top_side">
-        <iframe class="iframe_top" src="received_messages.php" frameborder="0" name="message_frame" id="message_frame" onload="adjustHeight(id); showUnreadCount(this); changeActiveClass(this);" ></iframe>
+        <iframe class="iframe" src="received_messages.php" frameborder="0" name="message_frame" id="message_frame" onload="showUnreadCount(this);" ></iframe>
+    </div>
+
+     <div class="div_popup_back">
+        <div class="div_popup popup_share">
+            <div class="popup_titlebar">New Message
+                <input type="button" class="popup_cancel white" id="popup_cancel" value="x">
+            </div>
+            <iframe id="popup_frame" name="popup_frame" src="compose_messages.php" frameborder="0"></iframe>
+        </div>
     </div>
 
     <form action="compose_messages.php" method="post" id="print_form" target="message_frame">
@@ -36,26 +45,8 @@ if (!isset($_SESSION["username"])) {
 
 <script type="text/javascript" src="//code.jquery.com/jquery-2.2.0.min.js"></script>
 <script>
-    function adjustHeight(iframe) {
-        var iframe = document.getElementById(iframe);
-        iframe.height = 0 + "px";
-        iframe.height = (document.body.scrollHeight - 48) + "px";
-    }
-
-    function changeActiveClass(iframe) {
-        if (iframe.contentDocument.title == "Compose") {
-            document.getElementById("inbox").className = "";
-            document.getElementById("deleted").className = "";
-            document.getElementById("compose").className = "active";
-        } else if(iframe.contentDocument.title == "Inbox") {
-            document.getElementById("deleted").className = "";
-            document.getElementById("compose").className = "";
-            document.getElementById("inbox").className = "active";
-        } else if (iframe.contentDocument.title == "Deleted") {
-            document.getElementById("inbox").className = "";
-            document.getElementById("compose").className = "";
-            document.getElementById("deleted").className = "active";
-        }
+    function composeButton() {
+        $(".div_popup_back").css("display", "block");
     }
 
     function showUnreadCount(iframe){
@@ -71,6 +62,17 @@ if (!isset($_SESSION["username"])) {
             }
         }
     }
+
+    $(document).ready(function() {
+        $("#popup_cancel").click(function() {
+            $(".div_popup_back").fadeOut(190, "linear");
+        });
+
+        $("a").click(function() {
+            $("a").removeClass("active");
+            $(this).addClass("active");
+        });
+    });
 </script>
 
 <?php if (isset($_POST["print_data"])): ?>
