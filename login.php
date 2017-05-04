be904c6b73de2957bf579e71eba545f0d0f881d7
Wasif Hussain <swash99@gmail.com>
2017-05-04 12:44:09 +0500

    Updated site version to 3.6.0

--- login.php
+++ login.php
@@ -41,6 +41,6 @@ if (isset($_POST["username"])) {
             </form>
         </div>
     </div>
-    <span class="version">v3.5.2</span>
+    <span class="version">v3.6.0</span>
 </body>
 </html>
